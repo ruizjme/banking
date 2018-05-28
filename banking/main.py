b1dd16c434d77ab42e7bb2e026eb91082659bf4e
Jaime Ruiz <jaimeruizno@gmail.com>
2018-05-28 22:52:44 +1000

    refactoring

--- banking/main.py
+++ banking/main.py
@@ -9,11 +9,13 @@ import os.path
 import csv
 import json
 import re
-from apiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+# from apiclient.discovery import build
+# from httplib2 import Http
+# from oauth2client import file, client, tools
 
+# custom modules in this directory
 import nab
+import sheets
 
 time0 = time.time()
 
@@ -25,15 +27,14 @@ nab.sign_in(user)
 nab.download_transactions()
 
 filePath = '/Users/Jaime/Downloads/TransactionHistory.csv'
-rows = []
-pat1 = re.compile(r'(V\d{4}) (\d\d/\d\d) ([\w\s]+) (\d+)')
-
 while not os.path.exists(filePath):
 	print("waiting for download to finish")
 	time.sleep(1)
 
 nab.quit()
 
+rows = []
+pat1 = re.compile(r'(V\d{4}) (\d\d/\d\d) ([\w\s]+) (\d+)')
 with open(filePath) as f:
 	reader = csv.reader(f)
 	for row in reader:
@@ -52,16 +53,7 @@ with open(filePath) as f:
 
 
 
-
-
-# Setup the Sheets API
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-store = file.Storage('credentials.json')
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-    creds = tools.run_flow(flow, store)
-service = build('sheets', 'v4', http=creds.authorize(Http()))
+service = sheets.setup()
 
 # Call the Sheets API
 SPREADSHEET_ID = '1XCtBEwkNBwKPZMphSFWvffTnGVRxwVkFycbcFC2mscc'
@@ -72,13 +64,19 @@ result = service.spreadsheets().values().update(
     spreadsheetId=SPREADSHEET_ID,
 	range=RANGE_NAME, body=body, valueInputOption='RAW'
 	).execute()
+
 print('{0} cells updated.'.format(result.get('updatedCells')))
 
+
+
+############ WIP: add amounts for certain categories
 coles = 0
 for row in rows:
 	if 'coles' in row[3].lower():
 		coles += float(row[1])
 
-print(coles)
-# driver.quit()
+print('Spent at Coles in the last 30 days (test):', coles)
+
+
+
 print('Total time: {} s'.format(time.time() - time0))
